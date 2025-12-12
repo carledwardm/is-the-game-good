10315aa4dc03bef8f1dbdf71a6f9b010caea7068
carledwardm <cmillardv77@gmail.com>
2025-12-12 18:09:06 -0500

    Add partial styling to header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Header />
         <h1 className={styles.tempHeading}>Home Page</h1>
       </main>
     </div>
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,40 @@
+'use client';
 import styles from "./Header.module.scss"
 import Image from "next/image";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export default function Header() {
     return <header className={styles.header}>
-        <Image src="/is-the-game-good-high-resolution-logo-transparent.png"
-        width={225}
-        height={40}
-        alt="Logo with controller icon"
-         />
+        <div className={styles.headerWrapper}>
+            {/* Logo */}
+            <Image src="/is-the-game-good-high-resolution-logo-transparent.png"
+            width={225}
+            height={40}
+            alt="Logo with controller icon"
+            />
+
+            {/* Nav */}
+            <div className={styles.navContainer}>                
+            {/* Header Search Bar */}
+            <nav className={styles.navBar}>
+                <a href="#" className={styles.navLink}>Home</a>
+                <a href="#" className={styles.navLink}>Games</a>
+                <a href="#" className={styles.navLink}>Sign Up</a>
+            </nav>
+            <nav className={styles.mobileNav}>
+                <GiHamburgerMenu className={styles.mobileNavIcon} />
+                <div className={styles.mobileNavLinks}>
+                    <a href="#"  className={styles.navLink}>Home</a>
+                    <a href="#"  className={styles.navLink}>Games</a>
+                    <a href="#"  className={styles.navLink}>Signup</a>
+                </div>
+            </nav>
+            <form onSubmit={event => event.preventDefault()} className={styles.gameSearch}>
+                <input type="text" className={styles.gameSearchInput} placeholder="Search Games"></input>
+                <button type="submit" className={styles.gameSearchButton}>Search</button>
+            </form>
+            </div>
+        </div>
     </header>
 }
 
