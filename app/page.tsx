2e7e706bda473256b0a284536e242df0cdc812ca
carledwardm <cmillardv77@gmail.com>
2025-12-12 15:35:29 -0500

    Add header div, Add color variables to globals, Add logo image file

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Home Page</h1>
+        <Header />
+        <h1 className={styles.tempHeading}>Home Page</h1>
       </main>
     </div>
   );
