31ba05154276d9bc8109e41f82d8030299f8f425
carledwardm <cmillardv77@gmail.com>
2025-12-12 15:45:00 -0500

    Add image component for logo in header, add sizing

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,14 @@
 import styles from "./Header.module.scss"
+import Image from "next/image";
 
 export default function Header() {
-    return <header className={styles.header}>Is the game good?</header>
+    return <header className={styles.header}>
+        <Image src="/is-the-game-good-high-resolution-logo-transparent.png"
+        width={225}
+        height={40}
+        alt="Logo with controller icon"
+         />
+    </header>
 }
 
 
