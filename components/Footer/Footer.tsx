83f046210cbb89c7b2b6d7d9b50979faa3d6b31f
carledwardm <cmillardv77@gmail.com>
2025-12-15 16:24:53 -0500

    Add basic footer container

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.scss';
+
+function footer() {
+    return <footer className={styles.footer}>
+        <h1>Footer!</h1>
+    </footer>
+}
+
+export default footer;
