d9ba3cb9e2a6f1bc05ffcadbb82cab50417a674d
carledwardm <cmillardv77@gmail.com>
2025-12-18 17:39:42 -0500

    Add styling to game add page, add logging for file and text submission for debugging

--- app/add-game/page.tsx
+++ app/add-game/page.tsx
@@ -1,7 +1,37 @@
+"use client";
 import styles from "./addGame.module.scss";
+import { useState } from "react";
 
 export default function addGameContainer() {
+    const [gameTitle, setGameTitle] = useState<string>("");
+    const [gameBoxArtFile, setGameBoxArtFile] = useState<File | null>(null)
+    const [gameScreenShotFiles, setGameScreenShotFiles] = useState<FileList | null>(null)
+    const handleGameSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(gameTitle);
+        console.log(gameBoxArtFile);
+        if (gameScreenShotFiles) {
+            console.log(gameScreenShotFiles[0].name);
+            console.log("first log");
+            console.log(gameScreenShotFiles[1].name);
+            console.log("second log");
+        }
+    }
+
     return (
-        <h1>Add game here!</h1>
+        <main className={styles.addGameMain}>
+            <div className={styles.addGameContainer}>
+                <h1 className={styles.title}>Add Game (admin only)</h1>
+                <form className={styles.form} onSubmit={handleGameSubmit}>
+                    <label htmlFor="addGameTitleInput" className={styles.addGameLabel}>Game title</label>
+                    <input id={styles.addGameTitleInput} type="text" area-label="Game title input" placeholder="Enter title" onChange={(e) => setGameTitle(e.target.value)}></input> 
+                    <label htmlFor="addGameBoxArt" className={styles.addGameLabel}>Add box art file</label>
+                    <input id={styles.addGameBoxArt}type="file" className={styles.addGameFileInput} accept=".jpg, .jpeg, .png" onChange={(e) => setGameBoxArtFile(e.target.files?.[0] ?? null)}></input>
+                    <label htmlFor="addGameTitleInput" className={styles.addGameLabel}>Add game screenshots</label>
+                    <input type="file" className={styles.addGameFileInput} accept=".jpg, .jpeg, .png" onChange={(e) => setGameScreenShotFiles(e.target.files ?? null)} multiple></input>
+                    <button type="submit" className={styles.addGameButton}>Submit game</button>
+                </form>
+            </div>
+        </main>
     )
 }
--- app/game-page/page.tsx
+++ app/game-page/page.tsx
@@ -1,5 +1,8 @@
 import styles from "./GamePage.module.scss";
 
+
 export default function gamePage() {
+    return (
     <h1>Game page here!</h1>
+    )
 }
