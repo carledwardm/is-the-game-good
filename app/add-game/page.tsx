3eb0bec5f8ca9efb2a00a61c0103dd070de1b913
carledwardm <cmillardv77@gmail.com>
2025-12-23 13:38:52 -0500

    Updated html for future search submission

--- app/add-game/page.tsx
+++ app/add-game/page.tsx
@@ -7,11 +7,14 @@ import { useState } from "react";
 
 export default function addGameContainer() {
     const [gameTitle, setGameTitle] = useState<string>("");
-    const [gameBoxArtFile, setGameBoxArtFile] = useState<File | null>(null)
-    const [gameScreenShotFiles, setGameScreenShotFiles] = useState<File[]>([])
+    const [gameBoxArtURL, setGameBoxArtURL] = useState<string>("")
+    const [gameScreenShotURLs, setGameScreenShotURLs] = useState<string[]>([])
+    const [gameSearchLimit, setGameSearchLimit] = useState<number>(5)
 
     const handleGameSubmit = (e: React.FormEvent) => {
         e.preventDefault();
+        console.log(gameTitle)
+        console.log(`Will search for ${gameSearchLimit} titles.` )
     }
 
     return (
@@ -19,38 +22,11 @@ export default function addGameContainer() {
             <div className={styles.addGameContainer}>
                 <h1 className={styles.title}>Add Game (admin only)</h1>
                 <form className={styles.form} onSubmit={handleGameSubmit}>
-                    <label htmlFor="addGameTitleInput" className={styles.addGameLabel}>Game title</label>
-                    <input id={styles.addGameTitleInput} type="text" area-label="Game title input" placeholder="Enter title" onChange={(e) => setGameTitle(e.target.value)}></input> 
-                    <label htmlFor="addGameBoxArt" className={styles.addGameLabel}>Add box art file</label>
-                    <input id={styles.addGameBoxArt}type="file" className={styles.addGameFileInput} accept=".jpg, .jpeg, .png" onChange={(e) => setGameBoxArtFile(e.target.files?.[0] ?? null)}></input>
-                    <label htmlFor="addGameTitleInput" className={styles.addGameLabel}>Add game screenshots</label>
-                    <input 
-                    type="file" 
-                    className={styles.addGameFileInput} 
-                    accept=".jpg, .jpeg, .png" 
-                    onChange={(e) => {
-                                    const screenShots = Array.from(gameScreenShotFiles);
-                                    if (!e.target.files || e.target.files.length === 0) {
-                                        return;
-                                    }
-                                    if (e.target.files && e.target.files.length === 1) {
-                                        screenShots.push(e.target.files[0]);
-                                    } 
-                                    if (e.target.files && e.target.files.length > 1) {
-                                        for (let i = 0; i < e.target.files.length; i++) {
-                                            screenShots.push(e.target.files[i]);
-                                        }
-                                    }
-                                    setGameScreenShotFiles(screenShots);
-                                    }} multiple></input>
-                    <div className={styles.fileNameContainer}>
-                        {
-                            gameScreenShotFiles.map((file, index) => (
-                                <p key={index} className={styles.fileName}>{file.name}</p>
-                            ))
-                        }
-                    </div>
-                    <button type="submit" className={styles.addGameButton}>Submit game</button>
+                    <label htmlFor="addGameTitleInput" className={styles.searchGameLabel}>Game title</label>
+                    <input id={styles.searchGameTitleInput} type="text" area-label="Game title input" placeholder="Enter title" onChange={(e) => setGameTitle(e.target.value)}></input> 
+                    <label htmlFor="searchLimitInput" className={styles.searchGameLabel}>Search Limit</label>
+                    <input id={styles.searchLimitInput} type="number" min={5} max={50} step={5} value={gameSearchLimit} onChange={(e) => setGameSearchLimit(parseInt(e.target.value))}/>   
+                    <button type="submit" className={styles.searchGameButton}>Submit Search</button>
                 </form>
             </div>
         </main>
--- lib/twitchIGDBConfig.ts
+++ lib/twitchIGDBConfig.ts
@@ -1,3 +1,5 @@
 const twitchClientID=process.env.TWITCH_CLIENT_ID
 const twitchClientSecret=process.env.TWITCH_CLIENT_SECRET
 const twitchAccessToken=process.env.TWITCH_ACCESS_TOKEN
+
+// Access token generated on 12/12/25, good for 63-64 days
