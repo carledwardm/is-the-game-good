5d0d2d111efaccd7c12d4fccbc9c12fcfb0fd57f
carledwardm <cmillardv77@gmail.com>
2025-12-23 17:02:18 -0500

    Add search results being logged back in add game page from route

--- app/add-game/api/route.ts
+++ app/add-game/api/route.ts
@@ -3,7 +3,6 @@ import twitchCreds from '@/lib/twitchIGDBConfig';
 
 export async function POST(req: Request) {
     const data = await req.json();
-    console.log(data);
     const url = "https://api.igdb.com/v4/games";
     try {
         const response = await fetch(url, {
@@ -16,13 +15,11 @@ export async function POST(req: Request) {
         body: `search "${data.gameTitle}"; fields name, id, first_release_date, artworks.url, screenshots.url; limit ${data.gameSearchLimit}; `
     });
     if (!response.ok) {
-        throw new Error("Unable to complete search, please try again.");
+        return NextResponse.json({error: "No results found, please check your search parameters and try again"}, {status: 400});
     }
     const searchData = await response.json();
-    console.log(searchData);
-    return NextResponse.json(data)
+    return NextResponse.json(searchData)
     } catch (error) {
-        console.log(error);
         return NextResponse.json({error: "Failed to fetch IGDB data"}, { status: 500 })
     }
 }
--- app/add-game/page.tsx
+++ app/add-game/page.tsx
@@ -1,26 +1,51 @@
 "use client";
 import styles from "./addGame.module.scss";
 import { useState } from "react";
+import Toast from "@/components/Toast";
 
 
 // *** SET UP IGDB API ROUTE - ADD GAME PAGE LIKELY NOT NECESSARY ***
 
 export default function addGameContainer() {
     const [gameTitle, setGameTitle] = useState<string>("");
-    const [gameBoxArtURL, setGameBoxArtURL] = useState<string>("")
-    const [gameScreenShotURLs, setGameScreenShotURLs] = useState<string[]>([])
-    const [gameSearchLimit, setGameSearchLimit] = useState<number>(5)
+    const [gameBoxArtURL, setGameBoxArtURL] = useState<string>("");
+    const [gameScreenShotURLs, setGameScreenShotURLs] = useState<string[]>([]);
+    const [gameData, setGameData] = useState<string[]>([]);
+    const [gameSearchLimit, setGameSearchLimit] = useState<number>(5);
+    const [showToast, setShowToast] = useState<boolean>(false);
+    const [toastMessage, setToastMessage] = useState<string>("");
 
+    // Search for a specified number of games by title. Current limit is set to 50 from html number input below.
     const handleGameSearch = async (e: React.FormEvent) => {
         e.preventDefault();
         const trimmedTitle = gameTitle.trim();
-        await fetch('/add-game/api', {
+        if (!gameTitle) {
+            setShowToast(true);
+            setToastMessage("A game title is required to complete a search.");
+            return;
+        }
+        try {        
+            const response = await fetch('/add-game/api', {
             method: "POST",
             body: JSON.stringify({
                 gameTitle: trimmedTitle,
                 gameSearchLimit: gameSearchLimit,
-            })
-        })
+                })
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                setShowToast(true);
+                setToastMessage(data.error);
+                return;
+            }
+            setShowToast(true);
+            setToastMessage("Search complete, please view results before submitting.");
+            for (let i = 0; i < data.length; i++) {
+                console.log(data[i]);
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
@@ -33,6 +58,14 @@ export default function addGameContainer() {
                     <label htmlFor="searchLimitInput" className={styles.searchGameLabel}>Search Limit</label>
                     <input id={styles.searchLimitInput} type="number" min={5} max={50} step={5} value={gameSearchLimit} onChange={(e) => setGameSearchLimit(parseInt(e.target.value))}/>   
                     <button type="submit" className={styles.searchGameButton}>Submit Search</button>
+                     {showToast && (
+                        <Toast 
+                        message={toastMessage}
+                        onClose={() => {
+                            setToastMessage("");
+                            setShowToast(false);
+                        }}/>
+                    )}
                 </form>
             </div>
         </main>
