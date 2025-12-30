f6a93e85eaa06b96af3a36e6dbe7c772baf9dbc6
carledwardm <cmillardv77@gmail.com>
2025-12-30 11:47:23 -0500

    Edit next.congif.js file to use remote patterns instead of domains.

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.igdb.com']
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'images.igdb.com',
+      pathname: '/**',
+    }]
   },
   reactCompiler: true,
 };
