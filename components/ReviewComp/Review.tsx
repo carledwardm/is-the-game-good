db1bc06834eb0c30a17966f5e92f903977bf1876
carledwardm <cmillardv77@gmail.com>
2026-01-09 15:47:22 -0500

    Add styling to reviews

--- components/ReviewComp/Review.tsx
+++ components/ReviewComp/Review.tsx
@@ -1,17 +1,20 @@
+
+"use client";
+
 import styles from "./Review.module.scss";
 import type { Review } from "@/types/types";
 
 export default function ReviewComp ({review, isAuthor}: {review: Review, isAuthor: boolean}) { 
     return (
             <div className={styles.review}>
-                {isAuthor && <h2>Your Review</h2>}
-                {!isAuthor && <h2 className={styles.autherName}>{review.authorUserName}</h2>}
-                <p className={styles.reviewScore}>{review.gameScore}</p>
+                {isAuthor && <h2 className={styles.authorName}>Your Review</h2>}
+                {!isAuthor && <h2 className={styles.authorName}>{`${review.authorUserName}'s Review`}</h2>}
+                <p className={styles.reviewScore}>{`${review.gameScore} / 100`}</p>
                 <p className={styles.reviewText}>{review.review}</p>
-                {isAuthor && <button>Delete</button>}
-                {!isAuthor && <button>Helpful?</button>}
+                {isAuthor && <button className={styles.deleteBtn}>Delete</button>}
+                {!isAuthor && <button className={styles.rateBtn}>Rate Review</button>}
             </div>
         )
 }
 
-// Will be improved - isAuthor will conditionally render a delete button, which can trigger a function to delete review passed in
+// Will be improved - improve buttons, add functions, add condition for user to only submit 1 review per game
