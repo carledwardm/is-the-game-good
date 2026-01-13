67804450644662d5f8117cc4651456b606835242
carledwardm <cmillardv77@gmail.com>
2026-01-13 16:02:04 -0500

    Convert usernames on reviews to profile page links, add styling

--- components/ReviewComp/Review.tsx
+++ components/ReviewComp/Review.tsx
@@ -24,8 +24,8 @@ export default function ReviewComp ({reviewData, isAuthor, onDelete}: {reviewDat
 
     return (
             <div className={isAuthor? `${styles.userReview} ${styles.review}` : styles.review}>
-                {isAuthor && <h2 className={styles.authorName}>Your Review</h2>}
-                {!isAuthor && <h2 className={styles.authorName}>{`${review.authorUserName}'s Review`}</h2>}
+                {isAuthor && <a  className={styles.profileLink} href={`/user-profile/${review.authorId}`} aria-label="Link to user profile page"><h2 className={styles.authorName}>Your Review</h2></a>}
+                {!isAuthor && <a className={styles.profileLink} href={`/user-profile/${review.authorId}`} aria-label="Link to user profile page"><h2 className={styles.authorName}>{`${review.authorUserName}'s Review`}</h2></a>}
                 <p className={styles.reviewScore}>{`${review.gameScore} / 100`}</p>
                 <p className={styles.reviewText}>{review.review}</p>
                 {isAuthor && <button className={styles.deleteBtn} onClick={deleteReview}>Delete</button>}
