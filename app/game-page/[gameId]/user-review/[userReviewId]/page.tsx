e929c03c39de60619e99e73a0a46ca4e11fc67d7
carledwardm <cmillardv77@gmail.com>
2026-01-19 12:12:02 -0500

    Add comment form, styling for user comment page, placeholder for function for submitting review

--- app/game-page/[gameId]/user-review/[userReviewId]/page.tsx
+++ app/game-page/[gameId]/user-review/[userReviewId]/page.tsx
@@ -12,7 +12,8 @@ export default function userReviewPage() {
     const { gameId, userReviewId } = useParams();
     const { user } = useAuth();
     const [ reviewDoc, setReviewDoc ] = useState<DocumentSnapshot<DocumentData>>();
-    const [ reviewData, setReviewData ] = useState<DocumentData | undefined>(undefined)
+    const [ reviewData, setReviewData ] = useState<DocumentData | undefined>(undefined);
+    const [ userComment, setUserComment ] = useState<string>("");
 
     useEffect(() => {
         console.log(typeof(gameId), gameId);
@@ -31,18 +32,40 @@ export default function userReviewPage() {
         fetchReviewData();
     }, [userReviewId, gameId])
 
+    const submitComment = async (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("Submitting comment!");
+    }
+
     return (
         <main className={styles.reviewMain}>
-            <h1 className={styles.reviewHeading}>
-                {reviewData?.authorId === user?.uid ? 
-                (<>
-                Your <span className={styles.reviewTitle}>{reviewData?.title}</span> review
-                </>) : (<>
-                    {reviewData?.authorUserName}'s 
-                    <span className={styles.reviewTitle}>{reviewData?.title} review</span>
-                </>)}
-            </h1>
-            <ReviewComp reviewData={reviewDoc} hideCommentButton={true} />
+            <div className={styles.reviewCommentsContainer}>
+                <h1 className={styles.reviewHeading}>
+                    {reviewData?.authorId === user?.uid ? 
+                    (<>
+                    Your <span className={styles.reviewTitle}>{reviewData?.title}</span> review
+                    </>) : (<>
+                        {reviewData?.authorUserName}'s 
+                        <span className={styles.reviewTitle}>{reviewData?.title} review</span>
+                    </>)}
+                </h1>
+                <div className={styles.reviewRow}>
+                    <ReviewComp reviewData={reviewDoc} hideCommentButton={true} />
+                </div>
+
+                <div className={styles.commentsRow}>
+                        {/* Comments will display here */}
+                </div>
+            </div>
+
+            <div className={styles.leaveCommentContainer}>
+                <form className={styles.commentForm}  onSubmit={submitComment}>
+                        <label htmlFor="commentInput" className={styles.inputLabel}>Leave a Comment</label>
+                        <textarea id={styles.commentInput} onChange={(e) => {setUserComment(e.target.value)}}></textarea>
+                        <button className={styles.submitBtn} type="submit">Leave Comment</button>
+                </form>
+            </div>
+
         </main>
         
     )
