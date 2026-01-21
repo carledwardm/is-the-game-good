166391cf659e4a652e899e8a4a2fc06516947ee2
carledwardm <cmillardv77@gmail.com>
2026-01-21 16:42:53 -0500

    Add toast logic to user review page for comments, add condition to render loading text

--- app/game-page/[gameId]/user-review/[userReviewId]/page.tsx
+++ app/game-page/[gameId]/user-review/[userReviewId]/page.tsx
@@ -7,13 +7,16 @@ import { useEffect, useState } from "react";
 import styles from "../UserReview.module.scss"
 import ReviewComp from "@/components/ReviewComp/ReviewComp";
 import ReviewRating from "@/components/ReviewComp/ReviewRating/ReviewRating"; 
+import Toast from "@/components/Toast";
 
 export default function userReviewPage() {
     const { gameId, userReviewId } = useParams();
-    const { user } = useAuth();
+    const { user, loading } = useAuth();
     const [ reviewDoc, setReviewDoc ] = useState<DocumentSnapshot<DocumentData>>();
     const [ reviewData, setReviewData ] = useState<DocumentData | undefined>(undefined);
     const [ userComment, setUserComment ] = useState<string>("");
+    const [ showToast, setShowToast ] = useState<boolean>(false);
+    const [ toastMessage, setToastMessage ] = useState<string>("");
 
     useEffect(() => {
         console.log(typeof(gameId), gameId);
@@ -26,15 +29,18 @@ export default function userReviewPage() {
                 setReviewDoc(reviewDoc);
                 setReviewData(reviewData);
             } catch (error) {
-                console.log(error);
+                setShowToast(true);
+                setToastMessage("An error has occurred fetching the review data, please try again");
             }
         }
         fetchReviewData();
-    }, [userReviewId, gameId])
+    }, [userReviewId, gameId]);
 
     const submitComment = async (e: React.FormEvent) => {
         e.preventDefault();
         if (!user) {
+            setShowToast(true);
+            setToastMessage("You must be logged in to leave comments.");
             return;
         }
         const commentRef = doc(db, "games", gameId as string, "reviews", userReviewId as string, "comments", user?.uid);
@@ -44,14 +50,16 @@ export default function userReviewPage() {
                 authorId: user?.uid,
             });
         } catch (error) {
-            console.log(error);
+            setShowToast(true);
+            setToastMessage("An error has occurred, please try again.");
         }
     }
 
     return (
         <main className={styles.reviewMain}>
             <div className={styles.reviewCommentsContainer}>
-                <h1 className={styles.reviewHeading}>
+                {loading || !reviewData ? <h1 className={styles.reviewHeading}>Loading...</h1> :  
+                    <h1 className={styles.reviewHeading}>
                     {reviewData?.authorId === user?.uid ? 
                     (<>
                     Your <span className={styles.reviewTitle}>{reviewData?.title} </span>review
@@ -60,6 +68,8 @@ export default function userReviewPage() {
                         <span className={styles.reviewTitle}>{reviewData?.title} </span>review
                     </>)}
                 </h1>
+                }
+                
                 <div className={styles.reviewRow}>
                     <ReviewComp reviewData={reviewDoc} hideCommentButton={true} showHelpfulButton={true}/>
                 </div>
@@ -76,7 +86,15 @@ export default function userReviewPage() {
                         <button className={styles.submitBtn} type="submit">Leave Comment</button>
                 </form>
             </div>
-
+            {showToast && (
+                        <Toast
+                        message={toastMessage}
+                        onClose={() => {
+                            setShowToast(false);
+                            setToastMessage("");
+                        }}
+                        ></Toast>
+                    )}            
         </main>
         
     )
