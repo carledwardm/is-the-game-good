e40c5a7894c4305079a32f323158f59cef6de722
carledwardm <cmillardv77@gmail.com>
2026-01-26 16:01:19 -0500

    Add artwork to GameComp, add sizing

--- components/games/gameComp/gameComp.tsx
+++ components/games/gameComp/gameComp.tsx
@@ -1,5 +1,6 @@
 import { DocumentData, DocumentSnapshot } from "firebase/firestore"
 import styles from "./gameComp.module.scss"
+import Image from "next/image";
 
 
 export default function gameComp ({ gameSnap } : {gameSnap: DocumentSnapshot<DocumentData>}) {
@@ -9,11 +10,18 @@ export default function gameComp ({ gameSnap } : {gameSnap: DocumentSnapshot<Doc
         <a  className={styles.wrappingLink} href={`/game-page/${gameData?.gameId}`} aria-label={`Link to ${gameData?.gameName ?? 'game'} info page`}>
             <div className={styles.gameInfoContainer}>
             {/* Image tag will go here */}
-                <div className={styles.gameNameAndScoreContainer}>
-                    <p className={styles.gameName}>{gameData?.name}</p>
-                    {/* NEED TO ADD REVIEW SCORE TO GAME DATA */}
-                    <p className={styles.gamecore}>{gameData?.reviewScore}</p>
-                </div>
+            <div className={styles.gameArt}>
+                <Image 
+                    src={`https:${gameData?.artwork}`}
+                    alt={`${gameData?.name} artwork`}
+                    fill
+                />
+            </div>
+            <div className={styles.gameNameAndScoreContainer}>
+                <p className={styles.gameName}>{gameData?.name}</p>
+                {/* NEED TO ADD REVIEW SCORE TO GAME DATA */}
+                <p className={styles.gamecore}>{gameData?.reviewScore}</p>
+            </div>
             </div>
         </a>
     )
