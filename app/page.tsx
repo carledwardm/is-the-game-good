60bc4fc77aaab52add8a6c0deacb539a0bb3e57d
carledwardm <cmillardv77@gmail.com>
2026-01-27 17:29:31 -0500

    Add hero section and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 import styles from "./page.module.css";
 import { useAuth } from "@/context/AuthContext"; 
+import HeroSection from "@/components/HomePage/HeroSection";
 
 export default function Home() {
-  const { user, loading } = useAuth();
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        {!loading ? (!user ? (
-          <h1 className={styles.tempHeading}>Home Page</h1>
-        ) : <h1 className={styles.tempHeading}>Welcome, {user.displayName}!</h1>) : <h1 className={styles.tempHeading}>Loading...</h1>}
+        <HeroSection />
       </main>
     </div>
   );
---
+++ components/HomePage/HeroSection/HeroSection.tsx
@@ -0,0 +1,16 @@
+import styles from "./HeroSection.module.scss";
+import { useAuth } from "@/context/AuthContext"; 
+
+export default function HeroSection() {
+    const { user } = useAuth();
+
+    return (
+        <section className={styles.heroSection}>
+            <div className={styles.textContainer}><h1 className={styles.heroTitle}>Is The Game Good?</h1>
+                <p className={styles.heroDescription}>Review games - or leave comments and likes on reviews!</p>
+                { user && <p className={styles.greeting}>Welcome, {user.displayName}!</p> }
+            </div>
+        </section>
+    )
+    
+}
