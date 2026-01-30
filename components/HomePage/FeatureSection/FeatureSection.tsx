26d6293da7e82d8293daa050ad452bd922725d01
carledwardm <cmillardv77@gmail.com>
2026-01-30 12:20:50 -0500

    Edit text and style feature section

--- components/HomePage/FeatureSection/FeatureSection.tsx
+++ components/HomePage/FeatureSection/FeatureSection.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 export default function FeatureSection() {
     return (
-        <section className={styles.featuresSection}>
+        <section className={styles.featureSection}>
             <div className={styles.featureContainer}>
                 <div className={styles.textContainer}>
-                    <h2>See what gamers really think</h2>
-                    <p>Every score comes from actual players like you - cutting through the noise of paid promotions and reviews.
+                    <h2 className={styles.featureTitle}>See what gamers really think</h2>
+                    <p className={styles.featureText}>Every score comes from actual players like you - cutting through the noise of paid promotions and reviews.
                         Trust the voices that matter the most - the players! 
                     </p>
                 </div>
@@ -24,19 +24,18 @@ export default function FeatureSection() {
                 <div className={styles.imageContainer}>
                      <Image 
                         className={styles.image} 
-                        src="/fightstick.jpg"
+                        src="/fightstick.png"
                         fill
                         alt="Arcade fightstick with yellow joystick and blue buttons"
                     />     
                 </div>
                 <div className={styles.textContainer}>
-                    <h2>See what gamers really think</h2>
-                    <p>Every score comes from actual players like you - cutting through the noise of paid promotions and reviews.
-                        Trust the voices that matter the most - the players! 
+                    <h2 className={styles.featureTitle}>Leave feedback on other player reviews</h2>
+                    <p className={styles.featureText}>Leave a comment on each review, or leave a helpful "like" to let users know if the review was 
+                        actually useful when deciding to purchase a game or not. 
                     </p>
                 </div>
             </div>
-            <div className={styles.featureContainer}></div>
         </section>
     )
 }
