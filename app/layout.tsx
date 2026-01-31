6d1a0ce9d2aa2b298dd620baf8e633859d7a310e
carledwardm <cmillardv77@gmail.com>
2026-01-31 12:52:14 -0500

    Add meta deta head for viewport width

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,9 @@ export default function RootLayout({
   children,}: Readonly<{ children: React.ReactNode;}>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} ${roboto.variable}`}>
         <AuthProvider >
           <Header />
